5d9f1c54a82df01b44b9c8891d5108b0a6f1c0a9
c1oud-dev <gksmf0187@gmail.com>
2026-05-14 16:20:29 +0900

    chore(#1): REDIS 의존성 추가

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -30,6 +30,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-json")
 
+    // Redis
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
     // Kotlin
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
