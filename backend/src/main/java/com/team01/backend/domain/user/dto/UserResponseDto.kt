21192917696cd9b8c263801569f9658e77e5e5a9
Yujin <ujin3261@gmail.com>
2026-05-15 16:09:22 +0900

    feat: 관리자-유저 조회 마이그레이션

---
+++ backend/src/main/java/com/team01/backend/domain/user/dto/UserResponseDto.kt
@@ -0,0 +1,21 @@
+package com.team01.backend.domain.user.dto
+
+import com.team01.backend.domain.user.entity.Role
+import com.team01.backend.domain.user.entity.User
+
+data class UserResponseDto(
+    val email: String,
+    val nickname: String,
+    val profileImage: String,
+    val role: Role
+) {
+    companion object {
+        fun from(user: User): UserResponseDto =
+            UserResponseDto(
+                email = user.email,
+                nickname = user.nickname,
+                profileImage = user.profileImage,
+                role = user.role
+            )
+    }
+}
