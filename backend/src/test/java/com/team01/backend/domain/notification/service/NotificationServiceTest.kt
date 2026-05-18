abdc5c354f406b8fd6c7fe4432df6de7f568317a
Yujin <ujin3261@gmail.com>
2026-05-19 02:16:38 +0900

    test: 알림 기능 테스트

--- backend/src/test/java/com/team01/backend/domain/notification/service/NotificationServiceTest.kt
+++ backend/src/test/java/com/team01/backend/domain/notification/service/NotificationServiceTest.kt
@@ -2,68 +2,102 @@ package com.team01.backend.domain.notification.service
 
 import com.team01.backend.domain.comment.dto.CommentRequestDto
 import com.team01.backend.domain.comment.service.CommentService
-import com.team01.backend.domain.notification.entity.Notification
 import com.team01.backend.domain.notification.repository.NotificationRepository
 import com.team01.backend.domain.notification.repository.SseEmitterRepository
 import com.team01.backend.domain.post.service.PostService
-import com.team01.backend.domain.user.entity.Role
-import com.team01.backend.domain.user.entity.User
-import com.team01.backend.domain.user.repository.UserRepository
+import com.team01.backend.domain.user.service.AuthService
 import jakarta.transaction.Transactional
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito
+import org.mockito.BDDMockito.given
+import org.mockito.Mockito.mock
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
-import org.springframework.test.annotation.Commit
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
-import java.util.List
 
 @SpringBootTest
 @ActiveProfiles("test")
 @Transactional
 @AutoConfigureMockMvc
-internal class NotificationServiceTest {
-    @Autowired
-    private val commentService: CommentService? = null
-
-    @Autowired
-    private val notificationRepository: NotificationRepository? = null
-
-    @Autowired
-    private val userRepository: UserRepository? = null
-
-    @Autowired
-    private val postService: PostService? = null
-
+internal class NotificationServiceTest @Autowired constructor(
+    private val commentService: CommentService,
+    private val notificationRepository: NotificationRepository,
+    private val postService: PostService,
+    private val authService: AuthService,
     @MockitoBean
-    private val emitterRepository: SseEmitterRepository? = null
+    private val emitterRepository: SseEmitterRepository
+) {
+    @BeforeEach
+    fun signUp(){
+        authService.signUp(
+            email = "commenter1@test.com",
+            password = "password1234",
+            nickname = "commenter1",
+        )
+    }
+    @AfterEach
+    fun tearDown() {
+        notificationRepository.deleteAllInBatch()
+//        emitterRepository.delete()
+    }
 
     @Test
-    @Commit
-    fun shouldNotifyPostOwnerWhenOtherUserComments() {
-        val commenter = userRepository!!.save<User>(
-            User("commenter@test.com", "pw", "commenter", null, Role.USER)
-        )
+    @DisplayName("댓글 달렸을 때 작성자에게 알림")
+    @Transactional
+    fun t1() {
 
-        val post = postService!!.write("user1@test.com", "title", "content", 1L, 2L) //user1
+        val post = postService.write("user1@test.com", "title", "content aaaaaaaa", 1L, 2L) //user1
+        val emitter = mock(SseEmitter::class.java)
+        given(emitterRepository.findByUserId(1L)).willReturn(listOf(emitter))
 
-        val emitter = Mockito.mock<SseEmitter>(SseEmitter::class.java)
+        // When
+        val comment = commentService.writeComment(post.id, CommentRequestDto("comment1", null), "commenter1@test.com")
 
-        Mockito.`when`<MutableList<SseEmitter>>(emitterRepository!!.findByUserId(1L))
-            .thenReturn(List.of<SseEmitter>(emitter))
+        // Then
+        val notifications = notificationRepository.findByReceiverIdOrderByCreatedAtDesc(1L)
 
-        commentService!!.writeComment(1L, CommentRequestDto("comment1", null), "commenter@test.com")
+        Assertions.assertThat(notifications).hasSize(1)
+        Assertions.assertThat(notifications[0].receiverId).isEqualTo(1L)
+    }
 
-        // then
-        val notifications: MutableList<Notification?> =
-            notificationRepository!!.findByReceiverIdOrderByCreatedAtDesc(1L)
+    @Test
+    @DisplayName("본인 글에 댓글 다는 경우 - 알림 X")
+    @Transactional
+    fun t2() {
 
-        Assertions.assertThat<Notification?>(notifications).hasSize(1)
-        Assertions.assertThat(notifications.get(0)!!.receiverId)
-            .isEqualTo(1L)
+        val post = postService.write("user1@test.com", "title", "content aaaaaaaa", 1L, 2L) //user1
+        val emitter = mock(SseEmitter::class.java)
+        given(emitterRepository.findByUserId(1L)).willReturn(listOf(emitter))
+        // When
+        val comment = commentService.writeComment(post.id, CommentRequestDto("comment1", null), "user1@test.com")
+
+        // Then
+        val notifications = notificationRepository.findByReceiverIdOrderByCreatedAtDesc(1L)
+
+        Assertions.assertThat(notifications).hasSize(0)
+    }
+    @Test
+    @DisplayName("답글 달렸을 때 댓글 작성자에게 알림")
+    @Transactional
+    fun t3() {
+        val post = postService.write("user1@test.com", "title", "content aaaaaaaa", 1L, 2L) //user1
+        val emitter = mock(SseEmitter::class.java)
+        given(emitterRepository.findByUserId(2L)).willReturn(listOf(emitter))
+
+        // When
+        val comment = commentService.writeComment(post.id, CommentRequestDto("comment1", null), "user2@test.com")
+        val reply = commentService.writeComment(post.id, CommentRequestDto("comment1", comment.id), "user3@test.com")
+
+        // Then
+        val notifications = notificationRepository.findByReceiverIdOrderByCreatedAtDesc(2L)
+
+        Assertions.assertThat(notifications).hasSize(1)
+        Assertions.assertThat(notifications[0].senderId).isEqualTo(3L)
     }
 }
