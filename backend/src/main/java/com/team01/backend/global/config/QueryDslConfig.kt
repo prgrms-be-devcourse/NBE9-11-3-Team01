1edaa11ce744391a59271a4fca6c745d1cad7e43
c1oud-dev <gksmf0187@gmail.com>
2026-05-18 11:38:17 +0900

    refactor(#21): QueryDslconfig 코틀린 전환

--- backend/src/main/java/com/team01/backend/global/config/QueryDslConfig.kt
+++ backend/src/main/java/com/team01/backend/global/config/QueryDslConfig.kt
@@ -1,20 +1,14 @@
-package com.team01.backend.global.config;
+package com.team01.backend.global.config
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import com.querydsl.jpa.impl.JPAQueryFactory
+import jakarta.persistence.EntityManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @Configuration
-@RequiredArgsConstructor
-public class QueryDslConfig {
-
-    private final EntityManager entityManager;
-
+class QueryDslConfig(
+    private val entityManager: EntityManager
+) {
     @Bean
-    public JPAQueryFactory jpaQueryFactory() {
-        return new JPAQueryFactory(entityManager);
-    }
+    fun jpaQueryFactory(): JPAQueryFactory = JPAQueryFactory(entityManager)
 }
-
