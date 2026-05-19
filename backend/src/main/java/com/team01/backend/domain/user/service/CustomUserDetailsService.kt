34867eba7b115d3071ba4a6ddbf25fc96b802fbd
csgangsai06512-ue <csgangsai06512@gmail.com>
2026-05-19 17:21:23 +0900

    user-api-optional delete

--- backend/src/main/java/com/team01/backend/domain/user/service/CustomUserDetailsService.kt
+++ backend/src/main/java/com/team01/backend/domain/user/service/CustomUserDetailsService.kt
@@ -12,8 +12,8 @@ import java.util.Collections
 class CustomUserDetailsService(private val userRepository: UserRepository) : UserDetailsService {
     
     /**
-     * 스프링 시큐리티의 입구 서블릿 필터와 연계되어 신원을 조회하는 코어 핵심 다리일세.
-     * 이메일을 기반으로 계정을 파악하고 권한 컬렉션을 안전하게 바인딩하여 반환하네.
+     * 스프링 시큐리티의 입구 서블릿 필터와 연계되어 신원을 조회하는 코어 핵심.
+     * 이메일을 기반으로 계정을 파악하고 권한 컬렉션을 안전하게 바인딩하여 반환.
      */
     override fun loadUserByUsername(email: String): UserDetails {
         val user = userRepository.findByEmail(email)
